81813344f2a9bf4c9ed2064469939c9a6efbf8f1
AHsofty <e.basaran2004@gmail.com>
2024-02-20 16:37:30 +0100

    Initial commit

---
+++ Backend/Aigis/src/main/java/org/example/aigis/AigisApplication.java
@@ -0,0 +1,13 @@
+package org.example.aigis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AigisApplication.class, args);
+    }
+
+}
