7d5dbbd7add906eca047bfc50ff0d88197927c2f
AHsofty <e.basaran2004@gmail.com>
2024-02-22 21:54:40 +0100

    Fix: Fixed an issue with the mapper trying to get images that don't exist

--- Backend/Aigis/src/main/java/org/example/aigis/Mapper/PostMapper.java
+++ Backend/Aigis/src/main/java/org/example/aigis/Mapper/PostMapper.java
@@ -6,6 +6,8 @@ import org.example.aigis.DTO.POST.PostGetDTO;
 import org.example.aigis.Model.Post;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 
 @Component
 @RequiredArgsConstructor
@@ -18,13 +20,18 @@ public class PostMapper {
     }
 
     public PostGetDTO fromEntityToPostGetDto(Post post) {
+        UUID imageId = null;
+        if (post.getImage() != null) {
+            imageId = post.getImage().getId();
+        }
+
         return PostGetDTO
                 .builder()
                 .content(post.getContent())
                 .likes(post.getLikes())
                 .date(post.getDate())
                 .id(post.getId())
-                .imageId(post.getImage().getId())
+                .imageId(imageId)
                 .build();
     }
 }
