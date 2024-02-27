778614023e38e2a4967d3e30fb2137cb007328cc
Eren <e.basaran2004@gmail.com>
2024-02-27 11:31:34 +0100

    Feat: created a posts component

---
+++ Front-end/Aigis/src/app/profile/post/post.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-post',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.scss'
+})
+export class PostComponent {
+
+}
