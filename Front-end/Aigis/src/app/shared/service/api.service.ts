fe6b96564ee620db4dc244a496692a5baab1907d
Eren <e.basaran2004@gmail.com>
2024-02-27 11:29:52 +0100

    Fix: fixed the GET request to get the posts

--- Front-end/Aigis/src/app/shared/service/api.service.ts
+++ Front-end/Aigis/src/app/shared/service/api.service.ts
@@ -11,75 +11,97 @@ import {PostModel} from "../../models/post.model";
 @Injectable({
   providedIn: 'root',
 })export class ApiService {
-  public static API_URL = 'http://localhost:8080/api/v1';
+    public static API_URL = 'http://localhost:8080/api/v1';
 
-  constructor(private http: HttpClient, private authService: AuthService) {
-  }
+    constructor(private http: HttpClient, private authService: AuthService) {
+    }
 
-  PostLogin(payload: { password: string; usernameUnique: string }) {
-    return this.http.post(`${ApiService.API_URL}/auth/login`, payload).pipe(
-      map((data) => {
-        return z
-          .object({
-            payload: z.object({
-              token: z.string(),
+    PostLogin(payload: { password: string; usernameUnique: string }) {
+        return this.http.post(`${ApiService.API_URL}/auth/login`, payload).pipe(
+            map((data) => {
+                return z
+                    .object({
+                        payload: z.object({
+                            token: z.string(),
+                        }),
+                    })
+                    .parse(data);
             }),
-          })
-          .parse(data);
-      }),
-      tap((data) => {
-        this.authService.setToken(data.payload.token);
-      })
-    );
-  }
-
-  PostRegister(payload: { usernameUnique: string; password: string, username: string}) {
-      return this.http.post(`${ApiService.API_URL}/user/register`, payload).pipe(
-          map((data) => {
-              return z
-                  .object({
-                      payload: z.object({
-                          token: z.string(),
-                      }),
-                  })
-                  .parse(data);
-          }),
-      );
-  }
-
-  GetUserById(id: string): Observable<UserModel> {
-    return this.http.get(`${ApiService.API_URL}/user/${id}`).pipe(
-      map((data) => {
-        return z
-          .object({
-            payload: z.object({
-              id: z.string(),
-              username: z.string(),
-              usernameUnique: z.string(),
-              about: z.string().nullable(),
-              role: z.string(),
-              profilePictureId: z.string().nullable(),
-            }),
-          })
-          .parse(data);
-      }),
-      map((data) => {
-        return new UserModel(
-          data.payload.id,
-          data.payload.username,
-          data.payload.usernameUnique,
-          data.payload.role,
-          data.payload.about || 'This user does not have a biography yet',
-        data.payload.profilePictureId || ''
+            tap((data) => {
+                this.authService.setToken(data.payload.token);
+            })
         );
-      })
-    );
-  }
+    }
 
-  public GetPostsByUserId(id: string): Observable<PostModel[]> {
-      let token = this.authService.getToken();
-      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-      return this.http.get<PostModel[]>(`${ApiService.API_URL}/post/${id}`, {headers: headers});
-  }
+    PostRegister(payload: { usernameUnique: string; password: string, username: string }) {
+        return this.http.post(`${ApiService.API_URL}/user/register`, payload).pipe(
+            map((data) => {
+                return z
+                    .object({
+                        payload: z.object({
+                            token: z.string(),
+                        }),
+                    })
+                    .parse(data);
+            }),
+        );
+    }
 
+    GetUserById(id: string): Observable<UserModel> {
+        return this.http.get(`${ApiService.API_URL}/user/${id}`).pipe(
+            map((data) => {
+                return z
+                    .object({
+                        payload: z.object({
+                            id: z.string(),
+                            username: z.string(),
+                            usernameUnique: z.string(),
+                            about: z.string().nullable(),
+                            role: z.string(),
+                            profilePictureId: z.string().nullable(),
+                        }),
+                    })
+                    .parse(data);
+            }),
+            map((data) => {
+                return new UserModel(
+                    data.payload.id,
+                    data.payload.username,
+                    data.payload.usernameUnique,
+                    data.payload.role,
+                    data.payload.about || 'This user does not have a biography yet',
+                    data.payload.profilePictureId || ''
+                );
+            })
+        );
+    }
+
+    public GetPostsByUserId(id: string): Observable<PostModel[]> {
+        let token = this.authService.getToken();
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+        return this.http.get<any>(`${ApiService.API_URL}/post/${id}`, {headers: headers}).pipe(
+            map(response => {
+                return response.payload.map((post: any) => {
+                    return z
+                        .object({
+                            id: z.string(),
+                            content: z.string(),
+                            likes: z.number(),
+                            date: z.number(),
+                            imageId: z.string().nullable(),
+                        })
+                        .parse(post);
+                });
+            }),
+            map((data: any[]) => {
+                return data.map((post: any) => new PostModel(
+                    post.id,
+                    post.content,
+                    post.likes,
+                    post.date,
+                    post.imageId || ''
+                ));
+            })
+        );
+    }
 }
