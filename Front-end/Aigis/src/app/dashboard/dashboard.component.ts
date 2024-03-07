1bedd43f5b896977abfca5418417dd7d940f0997
Eren <e.basaran2004@gmail.com>
2024-03-07 23:33:11 +0100

    Feat: added the UI to create a post

--- Front-end/Aigis/src/app/dashboard/dashboard.component.ts
+++ Front-end/Aigis/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,29 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ApiService} from "../shared/service/api.service";
+import {UserModel} from "../models/user.model";
+import {AuthService} from "../shared/service/auth.service";
+import {LucideAngularModule} from "lucide-angular";
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 
 export class DashboardComponent {
+  public user?: UserModel;
 
+
+  constructor(private apiService: ApiService, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.apiService.GetUserById(this.authService.getId()).subscribe((user: UserModel) => {
+      this.user = user;
+    });
+  }
 
 }
